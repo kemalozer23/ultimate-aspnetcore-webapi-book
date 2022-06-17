092efb24a1572c101aabcc541c6406ac4a45a291
kemalozer23 <89297908+kemalozer23@users.noreply.github.com>
2022-06-17 15:48:38 +0300

    before repository user interfaces and classes page43---pdfpage55

---
+++ my-app-0.API/ContextFactory/RepositoryContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Repository;
+
+namespace my_app_0.API.ContextFactory
+{
+    public class RepositoryContextFactory : IDesignTimeDbContextFactory<RepositoryContext>
+    {
+        public RepositoryContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var builder = new DbContextOptionsBuilder<RepositoryContext>().UseSqlServer(configuration.GetConnectionString("sqlConnection"), b => b.MigrationsAssembly("my-app-0.API"));
+
+            return new RepositoryContext(builder.Options);
+        }
+    }
+}
